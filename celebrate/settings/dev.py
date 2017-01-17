7fae6322c56d23c3df1f7a510ea93f593d135b77
J. Heasly <jheasly@earthlink.net>
2017-01-17 11:34:34 -0800

    dev settings tweak

--- celebrate/settings/dev.py
+++ celebrate/settings/dev.py
@@ -5,12 +5,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a*wo4+7u_w#il$_z()@m6!tse@u8@!m8n#h(e7xt*ed*p7j3+('
-
-
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 
 try:
     from .local import *
